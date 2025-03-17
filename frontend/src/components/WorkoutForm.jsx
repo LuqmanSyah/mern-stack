4d13ba15bd6ddbae183804e96f9c887477a72086
Luqman <89076043+LuqmanSyah@users.noreply.github.com>
2025-03-17 21:51:57 +0700

    Handling Error Response

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -8,6 +8,7 @@ const WorkoutForm = () => {
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
+  const [emptyFields, setEmptyFields] = useState([]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -26,6 +27,7 @@ const WorkoutForm = () => {
 
     if (!response.ok) {
       setError(json.error);
+      setEmptyFields(json.emptyFields);
     }
 
     if (response.ok) {
@@ -33,9 +35,10 @@ const WorkoutForm = () => {
       setLoad("");
       setReps("");
       setError(null);
+      setEmptyFields([]);
       console.log("new workout added", json);
 
-      dispatch({ type: 'CREATE_WORKOUT', payload: json})
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
 
@@ -43,13 +46,13 @@ const WorkoutForm = () => {
     <form className="create" onSubmit={handleSubmit}>
       <h3>Add a new Workout</h3>
       <label>Excersize Title: </label>
-      <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
+      <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} className={emptyFields.includes("title") ? "error" : ""} />
 
       <label>Load (in Kg): </label>
-      <input type="number" onChange={(e) => setLoad(e.target.value)} value={load} />
+      <input type="number" onChange={(e) => setLoad(e.target.value)} value={load} className={emptyFields.includes("load") ? "error" : ""} />
 
       <label>Reps: </label>
-      <input type="number" onChange={(e) => setReps(e.target.value)} value={reps} />
+      <input type="number" onChange={(e) => setReps(e.target.value)} value={reps} className={emptyFields.includes("reps") ? "error" : ""} />
 
       <button>Add Workout</button>
       {error && <div className="error">{error}</div>}
